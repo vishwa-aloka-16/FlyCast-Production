53c2f4e0af75ce69d79efc4baaef060ccc16c047
vishwaaloka16@gmail.com <vishwaaloka16@gmail.com>
2025-08-20 02:13:15 +0530

    feat: Update flight prediction API to render HTML template and remove old index.html

--- pred_model/flight_prediction_api.py
+++ pred_model/flight_prediction_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from flask_cors import CORS
 import pandas as pd
 import joblib
@@ -65,7 +65,7 @@ def predict():
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({'message': 'Flight Delay Prediction API is running.'})
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
